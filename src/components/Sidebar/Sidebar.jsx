8e4a74b70322f930db34970eb919afcb905afb5d
Vikas <provikas2001@gmail.com>
2023-06-08 16:01:34 +0530

    Added fixed sidebar to app

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,76 @@
+import {
+  CompassOutlined,
+  HeartOutlined,
+  HomeOutlined,
+  MessageOutlined,
+  PlusSquareOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { useState } from "react";
+import Logo from "../Logo/Logo";
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem("Home", "1", <HomeOutlined style={{ fontSize: "20px" }} />),
+  getItem("Search", "2", <SearchOutlined style={{ fontSize: "20px" }} />),
+  getItem("Explore", "3", <CompassOutlined style={{ fontSize: "20px" }} />),
+  getItem("Message", "4", <MessageOutlined style={{ fontSize: "20px" }} />),
+  getItem("Notification", "5", <HeartOutlined style={{ fontSize: "20px" }} />),
+  getItem("Create", "6", <PlusSquareOutlined style={{ fontSize: "20px" }} />),
+];
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+  return (
+    <Sider
+      theme="light"
+      style={{
+        width: 256,
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        paddingLeft: 20,
+        paddingTop: 40,
+        borderInlineEnd: "1px solid rgba(5, 5, 5, 0.08)",
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+      }}
+    >
+      <Logo />
+      <Menu
+        defaultSelectedKeys={["1"]}
+        mode="vertical"
+        theme="light"
+        inlineCollapsed={collapsed}
+        style={{
+          marginTop: "20px",
+          border: "none",
+        }}
+      >
+        {items.map((item) => (
+          <Menu.Item
+            key={item.key}
+            icon={item.icon}
+            style={{ fontSize: "16px", marginTop: "20px" }}
+          >
+            {item.label}
+          </Menu.Item>
+        ))}
+      </Menu>
+    </Sider>
+  );
+};
+export default Sidebar;
