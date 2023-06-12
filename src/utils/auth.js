2d1c084ddfe64e5788acda04466346995cbe5835
Vikas <provikas2001@gmail.com>
2023-06-12 23:23:38 +0530

    handling response getting from signup and login api

---
+++ src/utils/auth.js
@@ -0,0 +1,29 @@
+import store, { setAlert } from "../store/store";
+
+export const loginUser = (authFunction, values) => {
+  console.log(values);
+  authFunction(values)
+    .unwrap()
+    .then((res) => {
+      if (res.error) {
+        console.log(res.error);
+        store.dispatch(setAlert({ type: "error", content: res.error }));
+      } else if (res.token) {
+        const { token, data } = res;
+        localStorage.setItem("auth", JSON.stringify({ token, data }));
+        window.location.pathname = "/";
+      }
+    })
+    .catch((er) => {
+      console.log(er);
+
+      store.dispatch(
+        setAlert({
+          type: "error",
+          content: er.data.error,
+        })
+      );
+    });
+};
+
+export const signout = () => {};
