4c86a58536a96df6822d03204344a06f6326b5a3
Vikas <provikas2001@gmail.com>
2023-06-12 23:26:00 +0530

    Using alert component and slice to show alert

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import Explore from "./pages/Explore";
 import Messenger from "./pages/Messenger";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
+import store from "./store/store";
+import { useEffect, useState } from "react";
+import AlertMessage from "./utils/shared/component/Alert";
 function App() {
+  const [alertData, setAlertData] = useState(store.getState().alert.data);
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setAlertData(store.getState().alert.data);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/profile/:username" element={<Profile />} />
-      <Route path="/messenger" element={<Messenger />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/messenger" element={<Messenger />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      {alertData && (
+        <AlertMessage type={alertData.type} content={alertData.content} />
+      )}
+    </>
   );
 }
 
