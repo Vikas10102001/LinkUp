b4a2cf220c738db68e73ce17ce3856d2d6ba5d62
Vikas <provikas2001@gmail.com>
2023-06-12 23:15:53 +0530

    Changed logo from link to span

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function Logo() {
+export default function Logo({ additionalStyles }) {
   return (
-    <Link to="/" className="logo">
+    <span className="logo" style={additionalStyles}>
       LinkUp
-    </Link>
+    </span>
   );
 }
