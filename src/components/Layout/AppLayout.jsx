235e0ffde12d5c918ed76c6278a7ad152233107e
Vikas <provikas2001@gmail.com>
2023-06-12 23:16:18 +0530

    Updated App layout

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -2,10 +2,12 @@ import { Col, Row } from "antd";
 import Sidebar from "../Sidebar/Sidebar";
 const AppLayout = ({ children }) => (
   <Row>
-    <Col span={7}>
+    <Col span={7} className="layout-section_left">
       <Sidebar />
     </Col>
-    <Col span={17}>{children}</Col>
+    <Col span={17} className="layout-section_right">
+      {children}
+    </Col>
   </Row>
 );
 export default AppLayout;
