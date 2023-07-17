10975070f144e0a3f166069a14962a6d0e9c82fd
Vikas <provikas2001@gmail.com>
2023-07-17 15:45:17 +0530

    added post api for sending  post related requests

---
+++ src/store/api/post.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const postApi = createApi({
+  reducerPath: "postApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/api/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "Authorization",
+        `Bearer ${JSON.parse(localStorage.getItem("auth")).token}`
+      );
+      return headers;
+    },
+  }),
+  endpoints(builder) {
+    return {
+      createPost: builder.mutation({
+        query: (data) => {
+          return {
+            url: "/posts",
+            body: data,
+            method: "POST",
+          };
+        },
+      }),
+
+      getPost: builder.query({
+        query: () => {
+          return {
+            url: "/posts",
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
--- src/store/store.js
+++ src/store/store.js
@@ -4,20 +4,23 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import alertSlice from "./slice/alert";
 import userSlice from "./slice/user";
 import postSlice from "./slice/post";
+import { postApi } from "./api/post";
 const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [postApi.reducerPath]: postApi.reducer,
     alert: alertSlice.reducer,
     user: userSlice.reducer,
     post: postSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(authApi.middleware);
+    return getDefaultMiddleware().concat(authApi.middleware).concat(postApi.middleware);
   },
 });
 setupListeners(store.dispatch);
 export default store;
 export const { useSignupMutation, useSigninMutation } = authApi;
+export const { useCreatePostMutation, useGetPostQuery } = postApi;
 export const { setUser } = userSlice.actions;
 export const { setAlert } = alertSlice.actions;
 export const { setShowPostModal } = postSlice.actions;
