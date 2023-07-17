759aca6582def7048add1801a8421aed50fae625
Vikas <provikas2001@gmail.com>
2023-07-17 15:45:45 +0530

    changed reducer path for auth api

--- src/store/api/auth.js
+++ src/store/api/auth.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
-  reducerPath: "auth",
+  reducerPath: "authApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/api/v1" }),
   endpoints(builder) {
     return {
