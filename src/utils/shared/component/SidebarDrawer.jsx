818cebdf2e9f40547cd8ebfec695750042a530c9
Vikas <provikas2001@gmail.com>
2023-09-09 15:24:52 +0530

    fixed a bug

--- src/utils/shared/component/SidebarDrawer.jsx
+++ src/utils/shared/component/SidebarDrawer.jsx
@@ -1,9 +1,16 @@
 import { Drawer } from "antd";
 import React from "react";
 
-export default function SidebarDrawer({ children, title, open, setOpen }) {
+export default function SidebarDrawer({
+  children,
+  title,
+  open,
+  setOpen,
+  setCollapsed,
+}) {
   const handleSearchClose = () => {
     setOpen(false);
+    setCollapsed(false);
   };
   return (
     <Drawer
@@ -18,8 +25,7 @@ export default function SidebarDrawer({ children, title, open, setOpen }) {
         position: "relative",
         left: "81px",
         borderRadius: "0 20px 20px 0 ",
-        boxShadow:
-          "9px 2px 64px 1px rgba(0,0,0,0.2)",
+        boxShadow: "9px 2px 64px 1px rgba(0,0,0,0.2)",
       }}
       keyboard={true}
       maskClosable={true}
